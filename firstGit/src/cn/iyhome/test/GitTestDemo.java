d0e09c575b8c95ed988e8011e5aaff288a2aa6d8
enzo <2224548479@qq.com>
2019-07-29 17:22:19 +0800

    add test()

--- firstGit/src/cn/iyhome/test/GitTestDemo.java
+++ firstGit/src/cn/iyhome/test/GitTestDemo.java
@@ -4,4 +4,8 @@ public class GitTestDemo {
     public static void main(String[] args) {
         System.out.println("hello git...1");
     }
+
+    public static void test() {
+        System.out.println("test");
+    }
 }
