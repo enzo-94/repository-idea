9c1fda2ee946565d9b3a25e77674f39554f1a7b6
enzo <2224548479@qq.com>
2019-07-30 11:04:45 +0800

    update

--- firstGit/src/cn/iyhome/test/GitTestDemo.java
+++ firstGit/src/cn/iyhome/test/GitTestDemo.java
@@ -3,6 +3,7 @@ package cn.iyhome.test;
 public class GitTestDemo {
     public static void main(String[] args) {
         System.out.println("hello git...1");
+        System.out.println("hello git ...update");
     }
 
     public static void test() {
